8ae871d9ccf704f383ea8645a4b5f7ddb2f2cf1b
ananthrajsingh <ananthraj.singh@gmail.com>
2020-08-22 13:20:25 +0530

    Make search UI and list item for search recycler view

--- app/src/main/java/com/ananth/githubpeople/ui/main/SearchFragment.kt
+++ app/src/main/java/com/ananth/githubpeople/ui/main/SearchFragment.kt
@@ -18,7 +18,7 @@ class SearchFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        return inflater.inflate(R.layout.search_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
